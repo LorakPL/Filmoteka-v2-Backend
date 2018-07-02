6e8bb82dac200827484bdd88ffaa02b652c198fb
LorakPL <karolfur1995@gmail.com>
2018-07-02 17:54:08 +0200

    initial commit

---
+++ src/main/java/com/filmoteka/v2/backend/FilmotekaV2BackendApplication.java
@@ -0,0 +1,12 @@
+package com.filmoteka.v2.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaV2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmotekaV2BackendApplication.class, args);
+    }
+}
