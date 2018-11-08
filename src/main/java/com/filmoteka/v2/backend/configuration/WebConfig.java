5d140d54dda21db95abe39b3ee63f4c95fd65745
DESKTOP-R3VH6HI\piotr <piotrkaweckix@gmail.com>
2018-11-08 23:39:01 +0000

    added comment

--- src/main/java/com/filmoteka/v2/backend/configuration/WebConfig.java
+++ src/main/java/com/filmoteka/v2/backend/configuration/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    //TODO this is deprecated - will be changed when spring security will be introduced     
+    //TODO this is deprecated - will be changed when spring security will be introduced
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
